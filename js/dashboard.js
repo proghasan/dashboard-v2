428e659b929211d271c30bdd5c1fe8549647a1db
Hasan Sheikh <sheikharthur@gmail.com>
2021-03-27 00:55:55 +0600

    fixed design

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,6 +1,19 @@
 'use strict';
+
 let buttonClick = document.getElementById("toggle-sidebar");
 buttonClick.addEventListener('click', function() {
-    document.getElementById("sidebar").classList.toggle('active');
+    let sidebarActive = document.getElementById("sidebar").classList.toggle('active');
+    let loggedIn = document.getElementById("user-login-area");
+    let mainContent = document.getElementById("main-content");
+    // display hide when come mobile device
+    if(sidebarActive && screen.width  <= 768) {
+        loggedIn.style.display = 'none';
+        mainContent.style.display ='none'
+    }
+    else {
+        loggedIn.style.display = 'block';
+        mainContent.style.display ='block'
+    }
+
     document.getElementById("content").classList.toggle('active');
 })
